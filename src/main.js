21474f32a00614c9e554f1aa48bf3b6d6d6f2a6b
jp <617298698@qq.com>
2017-07-16 21:17:31 +0800

    rm jq

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import $ from 'jquery'
 import 'iview/dist/styles/iview.css'
 import Iview from './components/Iview'
 import bigMenu from './components/BigMenu'
